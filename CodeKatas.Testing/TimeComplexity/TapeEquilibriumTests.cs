d3befbdc774560bcae4702675baa08eef3dcdffe
Marc Stevenson <marc@nzfintech.co.nz>
2019-11-20 10:42:19 +1300

    TapeEquilibrium

---
+++ CodeKatas.Testing/TimeComplexity/TapeEquilibriumTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeKatas.Logic.TimeComplexity;
+using Xunit;
+
+namespace CodeKatas.Testing.TimeComplexity
+{
+    public class TapeEquilibriumTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 3, 1, 2, 4, 3, }, 1)]        
+        public void Test(int[] input, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, new TapeEquilibrium().Solve(input));
+        }
+    }
+}
