ccc55b8e621e00de6b7c2d0ad6edef1fec47afb4
Marc Stevenson <marc@nzfintech.co.nz>
2019-11-20 08:05:21 +1300

    Added PermutationMissingElement

---
+++ CodeKatas.Testing/TimeComplexity/PermutationMissingElementTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeKatas.Logic.TimeComplexity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeKatas.Testing.TimeComplexity
+{
+    public class PermutationMissingElementTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3 }, 2)]
+        [InlineData(new int[] { 1, 2, 3, 4, 6 }, 5)]
+        [InlineData(new int[] { 2, 3 }, 1)]
+        [InlineData(new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10, 11 }, 5)]
+        public void Test(int[] input, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, new PermutationMissingElement().Solve(input));
+        }
+    }
+}
