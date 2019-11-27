704b786b5faa21ea61f146b4ddf9439ce3a83c8e
Marc Stevenson <marc@nzfintech.co.nz>
2019-11-27 16:19:10 +1300

    FrogRiverOne

---
+++ CodeKatas.Logic/CountingElements/FrogRiverOne.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.Logic.CountingElements
+{
+    public class FrogRiverOne
+    {
+        /// <summary>
+        /// Find the earliest time when a frog can jump to the other side of the river
+        /// </summary>
+        /// <example>
+        /// A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position 0) and wants to get to the opposite bank (position X+1). Leaves fall from a tree onto the surface of the river.
+        /// 
+        /// You are given an array A consisting of N integers representing the falling leaves.A[K] represents the position where one leaf falls at time K, measured in seconds.
+        /// 
+        /// The goal is to find the earliest time when the frog can jump to the other side of the river.The frog can cross only when leaves appear at every position across the river from 1 to X (that is, we want to find the earliest moment when all the positions from 1 to X are covered by leaves). You may assume that the speed of the current in the river is negligibly small, i.e.the leaves do not change their positions once they fall in the river.
+        /// 
+        /// For example, you are given integer X = 5 and array A such that:
+        /// 
+        ///         A[0] = 1
+        ///       
+        ///         A[1] = 3
+        ///       
+        ///         A[2] = 1
+        ///       
+        ///         A[3] = 4
+        ///       
+        ///         A[4] = 2
+        ///       
+        ///         A[5] = 3
+        ///       
+        ///         A[6] = 5
+        ///       
+        ///         A[7] = 4
+        ///       In second 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the river.
+        /// 
+        ///       Write a function:
+        /// 
+        /// class Solution { public int solution(int X, int[] A); }
+        /// 
+        ///         that, given a non-empty array A consisting of N integers and integer X, returns the earliest time when the frog can jump to the other side of the river.
+        /// 
+        ///         If the frog is never able to jump to the other side of the river, the function should return −1.
+        /// 
+        /// For example, given X = 5 and array A such that:
+        /// 
+        ///   A[0] = 1
+        ///   A[1] = 3
+        ///   A[2] = 1
+        ///   A[3] = 4
+        ///   A[4] = 2
+        ///   A[5] = 3
+        ///   A[6] = 5
+        ///   A[7] = 4
+        /// the function should return 6, as explained above.
+        /// </example>
+        /// <see cref="https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/"/>
+
+        public int Solve(int x, int[] array)
+        {
+            var set = new HashSet<int>();            
+            var solutionSum = x * (x + 1) / 2;
+            var runningSum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var item = array[i];
+
+                if (set.Contains(item)) continue; // We don't care about duplicates
+
+                if (item > x || item < 1) continue; // We don't care about values over x or less than 1
+
+                set.Add(item);
+                runningSum += item;
+
+                // Does our 
+                if (solutionSum == runningSum) return i;
+            }
+
+            return -1; // No solution found
+        }
+    }
+}
---
+++ CodeKatas.Testing/CountingElements/FrogRiverOneTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeKatas.Logic.CountingElements;
+using Xunit;
+
+namespace CodeKatas.Testing.CountingElements
+{
+    public class FrogRiverOneTests
+    {
+        [Theory]
+        [InlineData(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6)]
+        [InlineData(5, new int[] { 1, 3, 1, 4, 2, 3, 4, 5, 4 }, 7)]
+        [InlineData(5, new int[] { 1, 3, 1, 1, 4, 2, 3, 4, 5, 4 }, 8)]
+        [InlineData(6, new int[] { 1, 3, 1, 1, 4, 2, 3, 4, 5, 4, 6, 1 }, 10)]
+        public void Test(int x, int[] input, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, new FrogRiverOne().Solve(x, input));
+        }
+    }
+}
