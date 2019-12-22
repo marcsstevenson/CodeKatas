abb304489cd2dc08529cf7ec83c5388d673e5a05
Marc Stevenson <marc@nzfintech.co.nz>
2019-12-23 12:20:50 +1300

    MinAvgTwoSlice

---
+++ CodeKatas.Logic/PrefixSums/MinAvgTwoSlice.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace CodeKatas.Logic.PrefixSums
+{
+    public class MinAvgTwoSlice
+    {
+        /// <summary>
+        /// A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
+        /// 
+        /// For example, array A such that:
+        /// 
+        ///     A[0] = 4
+        ///     A[1] = 2
+        ///     A[2] = 2
+        ///     A[3] = 5
+        ///     A[4] = 1
+        ///     A[5] = 5
+        ///     A[6] = 8
+        /// contains the following example slices:
+        /// 
+        /// slice(1, 2), whose average is (2 + 2) / 2 = 2;
+        /// slice(3, 4), whose average is (5 + 1) / 2 = 3;
+        /// slice(1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
+        /// The goal is to find the starting position of a slice whose average is minimal.
+        /// 
+        /// Write a function:
+        /// 
+        /// class Solution { public int solution(int[] A); }
+        /// 
+        ///         that, given a non-empty array A consisting of N integers, returns the starting position of the slice with the minimal average.If there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
+        /// 
+        ///        For example, given array A such that:
+        /// 
+        /// 
+        ///            A[0] = 4
+        ///        
+        ///            A[1] = 2
+        ///        
+        ///            A[2] = 2
+        ///        
+        ///            A[3] = 5
+        ///        
+        ///            A[4] = 1
+        ///        
+        ///            A[5] = 5
+        ///        
+        ///            A[6] = 8
+        ///        the function should return 1, as explained above.
+        /// 
+        /// 
+        ///        Write an efficient algorithm for the following assumptions:
+        /// 
+        /// 
+        ///        N is an integer within the range[2..100, 000];
+        ///         each element of array A is an integer within the range[−10, 000..10, 000].
+        /// </summary>
+        /// <see cref="https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/"/>
+        public int Solve(int[] a)
+        {
+            int minIndex = 0;
+            double minValue = double.PositiveInfinity;
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                var average = (a[i] + a[i + 1]) / 2.0;
+                if (average < minValue)
+                {
+                    minIndex = i;
+                    minValue = (a[i] + a[i + 1]) / 2.0;
+
+                }
+
+                // Try 3 elements up until index length - 2
+                if (i < a.Length - 2)
+                {
+                    average = (a[i] + a[i + 1] + a[i + 2]) / 3.0;
+                    if (average < minValue)
+                    {
+                        minIndex = i;
+                        minValue = average;
+                    }
+                }
+            }
+
+            return minIndex;
+        }
+        
+        public int[] GetPrefixSum(int[] inputArray)
+        {
+            var prefixSum = new int[inputArray.Length];
+
+            prefixSum[0] = inputArray[0];
+
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + inputArray[i];
+            }
+
+            return prefixSum;
+        }
+    }
+}
---
+++ CodeKatas.Testing/PrefixSums/MinAvgTwoSliceTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeKatas.Logic.PrefixSums;
+using Xunit;
+
+namespace CodeKatas.Testing.PrefixSums
+{
+    public class MinAvgTwoSliceTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 4, 2, 2, 5, 1, 5, 8 }, 1)]
+        public void Test(int[] a, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, new MinAvgTwoSlice().Solve(a));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 2, 2, 5, 1, 5, 8 }, new int[] { 4, 6, 8, 13, 14, 19, 27 })]
+        public void GetPrefixSumTest(int[] a, int[] expectedOutput)
+        {
+            var prefixSum = new MinAvgTwoSlice().GetPrefixSum(a);
+            Assert.Equal(expectedOutput, prefixSum);
+        }
+    }
+}
