a327b19bde94fcb0b2f6e467545a4575189885ce
Marc Stevenson <marc@nzfintech.co.nz>
2019-12-23 11:36:30 +1300

    GenomicRangeQuery further Tests

--- CodeKatas.Testing/PrefixSums/GenomicRangeQueryTests.cs
+++ CodeKatas.Testing/PrefixSums/GenomicRangeQueryTests.cs
@@ -8,8 +8,8 @@ namespace CodeKatas.Testing.PrefixSums
 
         [Theory]
         [InlineData("CAGCCTA", new int[] { 2, 5, 0 }, new int[] { 4, 5, 6 }, new int[] { 2, 4, 1 })]
-        //[InlineData("CAGCCTA", new int[] { 0, 3, 4, 4 }, new int[] { 2, 5, 4, 6 }, new int[] { 1, 2, 2, 1 })]
-        //[InlineData("CAGCCTA", new int[] { 3, 1, 0, 4 }, new int[] { 3, 4, 4, 5 }, new int[] { 2, 1, 1, 2 })]
+        [InlineData("CAGCCTA", new int[] { 0, 3, 4, 4 }, new int[] { 2, 5, 4, 6 }, new int[] { 1, 2, 2, 1 })]
+        [InlineData("CAGCCTA", new int[] { 3, 1, 0, 4 }, new int[] { 3, 4, 4, 5 }, new int[] { 2, 1, 1, 2 })]
         public void Test(string s, int[] p, int[] q, int[] expectedOutput)
         {
             Assert.Equal(expectedOutput, new GenomicRangeQuery().SolveSlowly(s, p, q));
