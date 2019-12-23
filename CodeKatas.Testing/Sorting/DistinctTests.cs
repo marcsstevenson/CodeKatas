28e9d7aa7c372b92d63ab20eeecbbe2c37d82607
Marc Stevenson <marc@nzfintech.co.nz>
2019-12-23 13:01:31 +1300

    Distinct

---
+++ CodeKatas.Testing/Sorting/DistinctTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeKatas.Logic.Sorting;
+using Xunit;
+
+namespace CodeKatas.Testing.Sorting
+{
+    public class DistinctTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 1, 1, 2, 3, 1 }, 3)]
+        [InlineData(new int[] { 1, 1, 3, 3, 1, 4, 8 }, 4)]
+        public void Test(int[] a, int expectedOutput)
+        {
+            Assert.Equal(expectedOutput, new Distinct().Solve(a));
+        }
+    }
+}
