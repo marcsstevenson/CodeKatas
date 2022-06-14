35e1fbd6607ad1f6f75fc37761688bbe34965159
Marc Stevenson <marcsstevenson@hotmail.com>
2022-06-14 17:04:41 +1200

    Added DoubleMaxSliceSum

--- CodeKatas.Logic/09-MaximumSliceProblem/DoubleMaxSliceSum.cs
+++ CodeKatas.Logic/09-MaximumSliceProblem/DoubleMaxSliceSum.cs
@@ -6,19 +6,28 @@ public class DoubleMaxSliceSum
 {
     public int solution(int[] A)
     {
-        int absoluteMax = A[0];
-        int localMax = A[0];
-        int nextSum;
-        int currentElement;
+        int[] slice1LocalMax = new int[A.Length];
+        int[] slice2LocalMax = new int[A.Length];
 
         for (int i = 1; i < A.Length; i++)
         {
-            currentElement = A[i];
-            nextSum = localMax + currentElement;
-            localMax = Math.Max(currentElement, nextSum);
-            absoluteMax = Math.Max(absoluteMax, localMax);
+            slice1LocalMax[i] = Math.Max(slice1LocalMax[i - 1] + A[i], 0);
         }
 
-        return absoluteMax;
+        //start from i=A.length-2 because slice can't end at index A.length-1
+        for (int i = A.Length - 2; i > 0; i--)
+        {
+            slice2LocalMax[i] = Math.Max(slice2LocalMax[i + 1] + A[i], 0);
+        }
+
+        int maxDoubleSliceSum = 0;
+
+        //compute sums of all slices to find absolute max
+        for (int i = 1; i < A.Length - 1; i++)
+        {
+            maxDoubleSliceSum = Math.Max(maxDoubleSliceSum, slice1LocalMax[i - 1] + slice2LocalMax[i + 1]);
+        }
+
+        return maxDoubleSliceSum;
     }
 }
---
+++ CodeKatas.Testing/09-MaximumSliceProblem/DoubleMaxSliceSumTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeKatas.Logic.MaximumSliceProblem;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeKatas.Testing.MaximumSliceProblem;
+
+public class DoubleMaxSliceSumTests
+{
+    [Theory]
+    [ClassData(typeof(TestDataProvider))]
+    public void Shall(int[] pA, int pExpected)
+    {
+        Assert.Equal(new DoubleMaxSliceSum().solution(pA), pExpected);
+    }
+
+    public class TestDataProvider : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new int[] { 3, 2, 6, -1, 4, 5, -1, 2 }, 17 };
+            yield return new object[] { new int[] { -2 - 3, 4, -1, -2, 1, 5, -3 }, 9 };
+            yield return new object[] { new int[] { 1, 2, 3 }, 0 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+}
